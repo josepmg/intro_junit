4fb5d9df1e36836479234c12e340ba9de3f77242
josepmg <josemello@id.uff.br>
2021-03-03 00:40:49 -0300

    Start implementing testProcessPaymentEqual, testProcessPaymentGreater and testProcessPaymentMinor tests.

--- test/pagamento/BilletProcessorTest.java
+++ test/pagamento/BilletProcessorTest.java
@@ -29,8 +29,8 @@ public class BilletProcessorTest {
 	public void testGetPaymentList() {
 		double expected = 0;
 		billetList.add(new Billet("0010001111001001000001", new Date(), 1500.00));
-		
-		for (Billet billet: billetList) {
+
+		for (Billet billet : billetList) {
 			expected += billet.getValue();
 		}
 
@@ -44,23 +44,70 @@ public class BilletProcessorTest {
 
 		Assertions.assertEquals(expected, totalValue);
 	}
-	
+
 	@DisplayName("Testa o somatório da lista de pagamentos")
 	@Test
 	public void testGetSumPaymentsValue() {
 		double expected = 0;
 		billetList.add(new Billet("0010001111001001000001", new Date(), 1500.00));
 		billetList.add(new Billet("0010001111001001000001", new Date(), 1000.00));
-		
-		for (Billet billet: billetList) {
+
+		for (Billet billet : billetList) {
 			expected += billet.getValue();
 		}
-		
+
 		ArrayList<Payment> paymentList = billetProcessor.getPaymentList(billetList);
-		
-		
+
 		Assertions.assertEquals(expected, billetProcessor.getSumPaymentsValue(paymentList));
 	}
+
+	@DisplayName("Testa se fatura está paga quando valor pago for maior que o valor da fatura")
+	@Test
+	public void testProcessPaymentGreater() {
+		double expected = 0;
+		billetList.add(new Billet("0010001111001001000001", new Date(), 1500.00));
+		billetList.add(new Billet("0010001111001001000001", new Date(), 1.00));
+
+		for (Billet billet : billetList) {
+			expected += billet.getValue();
+		}
+
+		ArrayList<Payment> paymentList = billetProcessor.getPaymentList(billetList);
+
+		Assertions.assertEquals(expected, billetProcessor.processPayment(invoice, billetList));
+	}
 	
+	@DisplayName("Testa se fatura está paga quando valor pago for igual ao valor da fatura")
+	@Test
+	public void testProcessPaymentEqual() {
+		double expected = 0;
+		billetList.add(new Billet("0010001111001001000001", new Date(), 500.00));
+		billetList.add(new Billet("0010001111001001000001", new Date(), 1000.00));
+
+		for (Billet billet : billetList) {
+			expected += billet.getValue();
+		}
+
+		ArrayList<Payment> paymentList = billetProcessor.getPaymentList(billetList);
+
+		Assertions.assertEquals(expected, billetProcessor.processPayment(invoice, billetList));
+	}
+	
+	@DisplayName("Testa se fatura está aberta quando valor pago for menor que o valor da fatura")
+	@Test
+	public void testProcessPaymentMinor() {
+		double expected = 0;
+		billetList.add(new Billet("0010001111001001000001", new Date(), 1400.00));
+		billetList.add(new Billet("0010001111001001000001", new Date(), 1.00));
+		billetList.add(new Billet("0010001111001001000001", new Date(), 9.00));
+
+		for (Billet billet : billetList) {
+			expected += billet.getValue();
+		}
+
+		ArrayList<Payment> paymentList = billetProcessor.getPaymentList(billetList);
+
+		Assertions.assertEquals(expected, billetProcessor.processPayment(invoice, billetList));
+	}
 
 }
